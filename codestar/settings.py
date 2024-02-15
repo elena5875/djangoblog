0b738442a32e9f2aa3a977017b38b81504bc480f
Elena freire <elenafreire75@gmail.com>
2024-02-15 20:06:03 +0000

    change setting debug to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-elena5875-djangoblog-5y5bybizqj6.ws-eu108.gitpod.io','.herokuapp.com']
 
